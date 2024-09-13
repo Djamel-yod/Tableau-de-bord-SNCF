ef45e6996b06e671dfd0e8c4a8f8a261d7bec611
Djamel-yod <isma1yoda@gmail.com>
2024-09-13 09:57:04 +0200

    Dashbord_NSCF

---
+++ README.md
@@ -0,0 +1,67 @@
+# Tableau de bord Analytique des Incidents de sécurité à la SNCF en 2023
+
+Dans ce projet, je développe un tableau de bord interactif sous Python pour analyser les incidents de sécurité à la SNCF en 2023. Je déploie ce tableau de bord sous un conteneur **Docker** pour faciliter son éventuelle déploiement sur le Cloud.
+
+
+## Contexte: 
+
+Le jeu de données que j'ai utilisé représente la description et la localisation des incidents de sécurité selon l’échelle de gravité de l’EPSF (Établissement public de sécurité ferroviaire) depuis janvier 2023.
+
+Historiquement, l’indicateur de pilotage des événements SEF (Sécurité de l'Exploitation Ferroviaire) à la SNCF était le nombre d’Évènements de Sécurité Remarquables (ESR), c’est-à-dire un évènement qui met en risque l’intégrité physique des personnes ou aux abords des installations ferroviaires sous couvert d’une défaillance du système ferroviaire (y compris les personnels, salariés de prestataires et sous-traitants, etc.).
+
+La SNCF a décidé à partir de janvier 2023 de suivre l’indicateur de pilotage l’EPSF afin de favoriser les comparaisons avec l’ensemble des exploitants ferroviaires et promouvoir une vision commune de la sécurité.
+
+ 
+
+L’échelle de gravité de l’EPSF comprend six niveaux :
+
+1 : Évènement « mineur » de sécurité
+
+2 : Évènement qui aurait pu avoir des conséquences matérielles / blessés légers
+
+3 : Évènement qui aurait pu avoir des conséquences humaines individuelles
+
+4 : Évènement qui aurait pu avoir des conséquences humaines collectives
+
+5 : Accident qui a eu des conséquences significatives
+
+6 : Accident qui a eu des conséquences graves
+
+## Objectif
+
+L'objectif du projet est de développer un tableau de bord sous **python** qui permet de synthétiser et de mettre en évidence les informations pertinentes sur les incidents de sécurité dans les gares SNCF pour l'année 2023.
+
+
+La base de données que nous avons exploitée est disponible sur le site opendata de la [SNCF](https://ressources.data.sncf.com/explore/dataset/incidents-de-securite-epsf/table/?sort=date&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJib3hwbG90IiwiZnVuYyI6IkNPVU5UIiwieUF4aXMiOiJncmF2aXRlX2Vwc2YiLCJzY2llbnRpZmljRGlzcGxheSI6dHJ1ZSwiY29sb3IiOiIjQTEwMDZCIiwiY2hhcnRzIjpbeyJmdW5jIjoiTUlOIiwieUF4aXMiOiJncmF2aXRlX2Vwc2YifSx7ImZ1bmMiOiJRVUFOVElMRVMiLCJ5QXhpcyI6ImdyYXZpdGVfZXBzZiIsInN1YnNldHMiOjI1fSx7ImZ1bmMiOiJRVUFOVElMRVMiLCJ5QXhpcyI6ImdyYXZpdGVfZXBzZiIsInN1YnNldHMiOjUwfSx7ImZ1bmMiOiJRVUFOVElMRVMiLCJ5QXhpcyI6ImdyYXZpdGVfZXBzZiIsInN1YnNldHMiOjc1fSx7ImZ1bmMiOiJNQVgiLCJ5QXhpcyI6ImdyYXZpdGVfZXBzZiJ9XX1dLCJ4QXhpcyI6ImRhdGUiLCJtYXhwb2ludHMiOiIiLCJ0aW1lc2NhbGUiOiJtb250aCIsInNvcnQiOiIiLCJjb25maWciOnsiZGF0YXNldCI6ImluY2lkZW50cy1kZS1zZWN1cml0ZS1lcHNmIiwib3B0aW9ucyI6eyJzb3J0IjoiZGF0ZSJ9fSwic2VyaWVzQnJlYWtkb3duIjoiIiwic2VyaWVzQnJlYWtkb3duVGltZXNjYWxlIjoiIn1dLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlLCJ0aW1lc2NhbGUiOiIifQ%3D%3D&calendarview=month).
+
+## Méthodologie
+
+
+- Prétraitement des données (Traitement des données manquantes, renommage de certaines modalités)
+
+- Structuration des bases de données qui vont servir à alimenter mon dashboard  
+
+- Tracer des graphiques avec la librairie **plotly**
+
+- Mise en place de l'application web avec la librairie **dash**
+
+
+## Résultats
+
+c:\Users\Admin\Videos\2024-09-13-08-46-34_rd48hjvM.mkv
+
+
+## Outils
+
+<img width="391" alt="Capture_Python2" src="https://github.com/Djamel-yod/Prediction-co2-France/assets/60408184/66deb372-f524-4d96-b982-7c7c39d8943b">
+
+
+<a href="#">#Tableau de Bord</a>
+<a href="#">#Pandas</a>
+<a href="#">#Plotly</a>
+<a href="#">#Dash</a>
+
+
+
+
+
---
+++ dashboard.py
@@ -0,0 +1,238 @@
+from dash import Dash, html, dcc, Input, Output, State
+import plotly.express as px
+import pandas as pd
+from itertools import chain
+
+
+
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+
+from urllib.request import urlopen
+import json
+import numpy as np
+import plotly.express as px
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB,
+                                           dbc.icons.BOOTSTRAP])
+# Chargement des données
+data=pd.read_csv("data.csv",sep=";")
+
+
+
+""" 
+          Prétraitement des données et tracé de la carte:
+          Cette carte est la repartitions du nombre d'incidents par régions.
+"""
+
+# Remplacer les abréviations de certaines régions par leurs noms
+data["Région"].replace(["RA","B","N","IdF"],["Auvergne-Rhône-Alpes","Bretagne","Normandie","Île-de-France"],inplace=True)
+
+# Compter le nombre d'incidents dans chaque régions
+df_counts=data["Région"].value_counts()
+
+# Création d'un dataframe avec pour colonnes les régions et le nombre d'incidents
+dict={"Région": df_counts.index.tolist(), "Nombre d'incidents":df_counts.values.tolist()}
+df_inc = pd.DataFrame(dict)
+
+# Ajout de la Corse qui n'existe pas dans les données de base. On va lui affecter la valeur 0
+df_inc.set_index("Région",inplace=True)
+df_inc.loc["Corse"] = 0
+df_inc.reset_index(inplace=True)
+
+# Récupération des données géographiques
+with urlopen('https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions-version-simplifiee.geojson') as response:
+    geojson = json.load(response)
+df_geo = pd.DataFrame([x['properties'] for x in geojson['features']])
+
+# Jointure des deux dataframes en utilisant la région comme clé (df des données brutes et df des coordonées géographiques des villes)
+df_final=df_geo.merge(df_inc,left_on="nom",right_on="Région")
+df_final=df_final[["nom","code","Nombre d'incidents"]]
+df_final['randNumCol'] = np.random.randint(0, 10, df_final.shape[0]).astype('str')
+
+# Figure
+fig1 = px.choropleth_mapbox(df_final, geojson=geojson, featureidkey='properties.nom', locations='nom',
+                        color="Nombre d'incidents", range_color=(0,41), center = {"lat":47, "lon":2},
+                        zoom=4.5, mapbox_style="carto-positron",
+                        opacity=0.5, labels={'nom':'Région'})
+
+fig1.update_layout(mapbox_style="open-street-map",
+                  showlegend=False,
+                  margin={"r":0,"t":0,"l":0,"b":0},
+                  width=600,
+                  height=400,
+                  )
+
+
+""" 
+          Prétraitement des données et tracé du graphique en bar qui sera la repartion des incidents
+          suivant leurs origines:
+"""
+
+# Compter le nombre d'incidents dans chaque régions
+df_counts_origin=data["Origine"].value_counts()
+
+# Création d'un dataframe avec pour colonnes les régions et le nombre d'incidents
+dict={"Origine": df_counts_origin.index.tolist(), "Nombre d'incidents":df_counts_origin.values.tolist()}
+df_counts_origin = pd.DataFrame(dict)
+
+
+# Tracer la Repartition des incidents en fonction de leurs origines
+fig2=px.bar(df_counts_origin, x='Origine', y="Nombre d'incidents", color="Origine")
+fig2.update_xaxes(showticklabels=False, title_text=None)
+fig2.update_layout(
+    title_text=None,
+    yaxis_title="Nombre d'incidents",
+)
+
+
+""" 
+          Prétraitement des données et tracé du graphique en camembert qui sera la repartion des incidents
+          suivant le niveau de gravité:
+"""
+
+# Création d'un dataframe avec pour colonnes les régions et le nombre d'incidents
+df_counts_gravite=data["Gravité EPSF"].value_counts()
+dict3={"Gravité": df_counts_gravite.index.tolist(), "Nombre d'incidents": df_counts_gravite.values.tolist()}
+df_counts_gravite=pd.DataFrame(dict3)
+
+# Figure de la repartition des incidents par gravité
+
+fig3= px.pie(df_counts_gravite, values="Nombre d'incidents", names="Gravité", hole=0.3)
+
+
+
+""" 
+          Prétraitement des données et tracé du graphique temporel au fil des mois de la gravité des incidents:
+"""
+
+
+df_counts_date=data["Date"].value_counts()
+dict3={"Date": df_counts_date.index.tolist(), "Nombre d'incidents": df_counts_date.values.tolist()}
+df_counts_date=pd.DataFrame(dict3)
+
+df_counts_date['Date'] = pd.to_datetime(df_counts_date['Date'])
+
+# Calcul de la somme des incidents par mois
+incidents_par_mois = df_counts_date.groupby(df_counts_date['Date'].dt.month)['Nombre d\'incidents'].sum()
+
+
+# Les labels de tout les mois de Janvier à Décembre
+
+month_labels = ["Janvier", "Février", "Mars", "Avril", "Mai",
+               "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+
+
+""" On constate que ce n'est pas tout les mois qui existe dans notre dataframe: Les mois vont de janvier à Aout.
+    Il est donc probable qu'il n'yai pas eu d'incidents de Septembre à Décembre. N'ayant pas les moyens 
+    de le vérifier, Nous allons considérer qu'il n'ya pas eu d'incidents ces mois.
+"""
+
+# Considérons que incidents_par_mois.index contient les mois existant dans notre dataframe
+
+# Créer un vecteur contenant tout les mois
+all_months = set(range(1, 13))
+
+# Différence entre le nombre de mois existant et le nombre de mois dans l'année
+missing_months = all_months.difference(incidents_par_mois.index)
+
+# Créer un serie d'index pour les mois inexistants
+missing_months_index = pd.Index(missing_months)
+
+# Concatener les deux serie  d'index (mois existant et inexistant)
+new_index = incidents_par_mois.index.union(missing_months_index)
+
+# Fill missing month values with 0 (or a different default value)
+incidents_par_mois = incidents_par_mois.reindex(new_index, fill_value=0)
+
+incidents_par_mois.index=month_labels
+
+fig4=px.line(df_counts_date, x=incidents_par_mois.index, y=incidents_par_mois.values)
+fig4.update_layout(
+    title_text=None,
+    yaxis_title="Nombre d'incidents",
+    xaxis_title="Mois"
+)
+
+
+
+"""
+Carte du nombre total d'incidents
+"""
+
+total_incidents= len(data["Nature"])
+
+
+
+app.layout = dbc.Container([
+    
+    dbc.Row([
+        dbc.Col(html.H1("Dashboard Analytique des Incidents de sécurité à la SNCF en 2023", 
+                        style={"textAlign":"center", "text-decoration-color": "#007bff","color": "blue",
+                               "textShadow": "2px 2px 4px #000", "fontSize": "48px", "backgroundColor": "White"}
+                        
+                        ), width=14,), 
+            
+            ],  style={'margin-bottom': '50px'}),
+ 
+    
+    dbc.Row([
+        
+        dbc.Col([
+        dbc.Card(
+    dbc.CardBody(
+        [
+            html.H3([html.I(className="bi bi-train-front me-2"),
+                     "Total Incidents"], className="text-nowrap", style={"textAlign":"center"}),
+            html.H1(total_incidents, style={"textAlign":"center", "color": "red"})
+        ], className="border-start border-success border-5", style={"background-color": "yellow"},
+                    ), 
+    
+                    ),
+                ], width=5, className="four columns"),
+        
+        
+        dbc.Col([ dbc.Card([
+            
+                dcc.Graph(figure=fig1),
+                
+                ], style={'border': '0'})
+               
+                
+                ], width=3, className="twelve columns"),
+        
+        
+         
+            ]),
+    
+    
+    dbc.Row([
+    dbc.Col([
+        dcc.Graph(figure=fig2)
+    ], className="four columns"),
+
+    dbc.Col([
+        dcc.Graph(figure=fig3)
+    ], className="four columns"),
+    
+    
+    dbc.Col([  dbc.Card([
+        
+        dcc.Graph(figure=fig4),
+        
+         ], style={'border': '0'})
+                 
+            ], className="four columns"),    
+
+            ]),
+
+
+
+        ])
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port='8080')
+
